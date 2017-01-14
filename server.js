664f37cb21f35ae7d6420f1afa435b5bb77f4497
Calube <caleb.e.davis@eagles.usm.edu>
2017-01-14 14:35:16 -0500

    added baseURL

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const MongoClient = require('mongodb').MongoClient
 var db 
 app.set('port', (process.env.PORT || 3000));
 
+var baseURL = '45.55.250.94'
+
+
 //For avoidong Heroku $PORT error
 app.get('/', function(request, response) {
     var result = 'App is running'
@@ -31,41 +34,41 @@ app.get('/', (req, res) => {
 
 // app.use(bodyParser.urlencoded({extended: true}))
 
-// app.post('/saveMaps', (req, res) => {
-// 	console.log(req.body)
-//   db.collection('maps').save(req.body, (err, result) => {
-//     if (err) return console.log(err)
+app.post(baseURL + '/saveMaps', (req, res) => {
+	console.log(req.body)
+  db.collection('maps').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
-//     console.log('saved to database')
-//     res.send('saved to database')
-//   })
-// })
+    console.log('saved to database')
+    res.send('saved to database')
+  })
+})
 
-// app.get('/getMaps', (req, res) => {
-// 	var cursor = db.collection('maps').find()
-// 	db.collection('maps').find().toArray(function(err, results) {
-//     if (err) return console.log(err)
+app.get(baseURL + '/getMaps', (req, res) => {
+	var cursor = db.collection('maps').find()
+	db.collection('maps').find().toArray(function(err, results) {
+    if (err) return console.log(err)
 
-//     console.log(results)
-// 	res.json(results)
-//   })
-// })
+    console.log(results)
+	res.json(results)
+  })
+})
 
-// app.post('/saveLocations', (req, res) => {
-// 	console.log("location!")
-// 	console.log(req.body)
-//   db.collection('locations').save(req.body, (err, result) => {
-//     if (err) return console.log(err)
-//     console.log('saved to database')
-//     res.send('saved to database')
-//   })
-// })
+app.post('/saveLocations', (req, res) => {
+	console.log("location!")
+	console.log(req.body)
+  db.collection('locations').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+    res.send('saved to database')
+  })
+})
 
-// app.get('/getLocations', (req, res) => {
-// 	var cursor = db.collection('locations').find()
-// 	db.collection('locations').find().toArray(function(err, results) {
-//     if (err) return console.log(err)
-//     console.log(results)
+app.get('/getLocations', (req, res) => {
+	var cursor = db.collection('locations').find()
+	db.collection('locations').find().toArray(function(err, results) {
+    if (err) return console.log(err)
+    console.log(results)
 // 	res.json(results)
 //   })
 // })
